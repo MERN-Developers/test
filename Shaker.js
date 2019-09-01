1351538915e3be27967e29429b23736c07f92228
Mohamad Aktham Obeidat <mohamadaktham7@gmail.com>
2019-09-01 13:53:11 +0300

    Create Shaker.js

---
+++ Shaker.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Shaker extends Component {
+    render() {
+        return (
+            <div>
+                <h1>SHAKEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEER</h1>
+            </div>
+        )
+    }
+}
